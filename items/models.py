286d251bf7cef60ae15f4520214bfeafd1bc4402
ibraheem areeda <125559748+ibraheem-areeda@users.noreply.github.com>
2023-07-23 14:21:55 +0300

    Update models.py

--- items/models.py
+++ items/models.py
@@ -1,9 +1,10 @@
 from django.db import models
-from django.contrib.auth import get_user_model
+from accounts.models import CustomUser
 from django.urls import reverse
 
 class item (models.Model):
     name = models.CharField(max_length=64)
+    purcheser = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     price = models.IntegerField(default= 1)
 
     def __str__(self):
